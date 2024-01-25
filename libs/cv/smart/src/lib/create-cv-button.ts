9a67c7ef4cff435fc61ae0ad868fa4dc7c61bd9b
Robby Rabbitman <robby.rabbitman@gmail.com>
2024-01-25 19:45:38 +0100

    chore: create btn

--- libs/cv/smart/src/lib/create-cv-button.ts
+++ libs/cv/smart/src/lib/create-cv-button.ts
@@ -14,12 +14,12 @@ import { Translate, provideTranslatePrefix } from '@cv/i18n/smart';
   imports: [Translate],
   viewProviders: [provideTranslatePrefix('CV.CREATE_BUTTON')],
   template: `<button
-    class="button"
+    class="button h-80 w-56 items-center flex-col justify-center"
     (click)="cv.create(template()!)"
     [disabled]="cv.loading()"
   >
-    {{ 'LABEL' | translate }}
-    <span aria-hidden="true" class="icon">add_circle</span>
+    <span class="text-xl">{{ 'LABEL' | translate }}</span>
+    <span aria-hidden="true" class="icon !text-7xl">add_circle</span>
   </button>`,
   styleUrl: './create-cv-button.scss',
   encapsulation: ViewEncapsulation.None,
