3afffb7d86c1763693a6ee3f5cd90630e10fd635
robert hasemann <robert.hasemann@hdi.de>
2024-12-08 22:59:50 +0100

    chore: setup basic ci

--- tools/workspace/src/setup-node.sh
+++ tools/workspace/src/setup-node.sh
@@ -5,15 +5,15 @@ echo "⚙️ Setting up node via nvm - using the node version specified in $(pwd
 node_version=$(jq -r '.engines.node // empty' package.json)
 
 if [ -z "$node_version" ]; then
-    echo '❌ No node version specified in package.json'
-    exit 1
+  echo '❌ No node version specified in package.json'
+  exit 1
 fi
 
 if [ -f "$NVM_DIR/nvm.sh" ]; then
-    . "$NVM_DIR/nvm.sh"
+  . "$NVM_DIR/nvm.sh"
 else
-    echo '❌ Unable to find nvm.sh in $NVM_DIR'
-    exit 1
+  echo '❌ Unable to find nvm.sh in $NVM_DIR'
+  exit 1
 fi
 
 nvm use $node_version || nvm install $node_version
