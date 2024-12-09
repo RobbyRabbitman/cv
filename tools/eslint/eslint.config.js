cf14fffeb2a3f60dbf1b96e0d5c50af488b66c1f
robby rabbitman <robby.rabbitman@gmail.com>
2024-12-10 00:25:24 +0100

    build: add eslint

--- tools/eslint/eslint.config.js
+++ tools/eslint/eslint.config.js
@@ -1,5 +1,3 @@
-// @ts-check
 import { nodeEslintConfig } from './dist/index.js';
 
-/** @type {import('eslint').Linter.Config[]} */
 export default [...nodeEslintConfig];
---
+++ tools/vitest/eslint.config.js
@@ -0,0 +1,3 @@
+import { nodeEslintConfig } from '@robby-rabbitman/cv-tools-eslint';
+
+export default [...nodeEslintConfig];
