aa27808ba9b1cf94b08b964c29dc4ce2e6a22f19
robby rabbitman <robby.rabbitman@gmail.com>
2024-12-09 23:34:32 +0100

    ci: enforce conventional commits

--- tools/workspace/src/clean-workspace.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-echo '⚙️ Cleaning workspace - removing all git ignored files, reinstalling npm packages and resetting the nx workspace'
-
-git clean -dfX
-pnpm i
-nx reset
--- tools/workspace/src/npm-life-cycle/npm-post-install.sh
+++ tools/workspace/src/npm-life-cycle/npm-post-install.sh
@@ -1,1 +1,12 @@
 #!/bin/sh
+
+for tool in "tools"/*; do
+    if [ -d "$tool" ]; then
+        tsconfig="$tool/tsconfig.lib.json"
+
+        if [ -f "$tsconfig" ]; then
+            echo "⚙️ Building $tool ..."
+            pnpm tsc --build $tsconfig
+        fi
+    fi
+done
