075fba544bc76a4ddc3d3bfe780f931dd2dd06fc
robby rabbitman <robby.rabbitman@gmail.com>
2024-12-13 19:47:10 +0100

    feat(@robby-rabbitman/cv-apps-web-cv): init

--- apps/web/cv/src/app/cv-app.config.ts
+++ apps/web/cv/src/app/cv-app.config.ts
@@ -8,11 +8,18 @@ import {
   withDebugTracing,
 } from '@angular/router';
 
+import { provideHttpClient } from '@angular/common/http';
 import { cvAppRoutes } from './cv-app.routes';
 
 export const cvAppConfig = {
   providers: [
+    /** Change detection */
     provideExperimentalZonelessChangeDetection(),
+
+    /** Router */
     provideRouter(cvAppRoutes, withDebugTracing(), withComponentInputBinding()),
+
+    /** Http client */
+    provideHttpClient(),
   ],
 } satisfies ApplicationConfig;
