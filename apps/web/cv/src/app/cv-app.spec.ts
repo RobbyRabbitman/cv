dd55939b1f6f7628cef8c03b2212f68918a86499
robby rabbitman <robby.rabbitman@gmail.com>
2024-12-13 19:21:33 +0100

    feat(@robby-rabbitman/cv-apps-web-cv): init

--- apps/web/cv/src/app/cv-app.spec.ts
+++ apps/web/cv/src/app/cv-app.spec.ts
@@ -1,6 +1,5 @@
 import { provideExperimentalZonelessChangeDetection } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
-import { expect } from 'chai';
 import { CvApp } from './cv-app';
 
 describe('[Unit Test] CvApp', () => {
@@ -14,7 +13,6 @@ describe('[Unit Test] CvApp', () => {
   it('should create', () => {
     const fixture = TestBed.createComponent(CvApp);
     const app = fixture.componentInstance;
-    expect(1).to.equal(2);
-    expect(app).to.be.instanceOf(CvApp);
+    expect(app).toBeInstanceOf(CvApp);
   });
 });
