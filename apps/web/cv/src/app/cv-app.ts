1b92ec38a484ddeeade1ffbc0363aa67be1f5044
robby rabbitman <robby.rabbitman@gmail.com>
2024-12-13 19:55:44 +0100

    feat(@robby-rabbitman/cv-apps-web-cv): init

--- apps/web/cv/src/app/cv-app.ts
+++ apps/web/cv/src/app/cv-app.ts
@@ -3,7 +3,10 @@ import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'cv',
-  template: `<router-outlet />`,
+  template: `123<router-outlet />`,
   imports: [RouterOutlet],
+  host: {
+    class: 'flex',
+  },
 })
 export class CvApp {}
