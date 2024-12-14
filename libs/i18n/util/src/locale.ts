a702b10c626e9dd5336374f0176796af05bace52
robby rabbitman <robby.rabbitman@gmail.com>
2024-12-14 01:38:46 +0100

    feat: introduce i18n

---
+++ libs/i18n/util/src/locale.ts
@@ -0,0 +1,48 @@
+import { Injector } from '@angular/core';
+import { injectWindow } from '@cv/common/util';
+import { assertInjector } from 'ngxtension/assert-injector';
+
+/** Injects the locale from the document. */
+export function injectDocumentLocale(injector?: Injector) {
+  injector = assertInjector(injectDocumentLocale, injector);
+  return injectWindow({ injector }).navigator.language;
+}
+
+export function coerceLocale(
+  locale: string,
+  locales: string[],
+  options: {
+    weights: Partial<Record<keyof Intl.LocaleOptions, number>>;
+  },
+) {
+  const localeParts = new Intl.Locale(locale);
+
+  const weights = {
+    language: 1, // Intl.Locale will always have a language
+    ...options.weights,
+  } satisfies typeof options.weights;
+
+  let bestMatch: string | undefined;
+  let bestMatchScore = -1;
+
+  for (const candidate of locales) {
+    const candidateParts = new Intl.Locale(candidate);
+
+    let score = 0;
+
+    for (const weight in weights) {
+      if (
+        localeParts[weight as keyof Intl.LocaleOptions] ===
+        candidateParts[weight as keyof Intl.LocaleOptions]
+      )
+        score += weights[weight as keyof Intl.LocaleOptions] ?? 0;
+    }
+
+    if (score > bestMatchScore) {
+      bestMatch = candidate;
+      bestMatchScore = score;
+    }
+  }
+
+  return bestMatch;
+}
