1374fff6139309987ce5425ef3513a2fca7e6dc7
robby rabbitman <robby.rabbitman@gmail.com>
2024-12-20 00:06:43 +0100

    .

--- libs/common/feature--shell/src/header/header.spec.ts
+++ libs/common/feature--shell/src/header/header.spec.ts
@@ -1,6 +1,8 @@
+import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed';
 import { TestBed } from '@angular/core/testing';
 import { provideCommonData } from '@robby-rabbitman/cv-libs-common-data';
 import { provideCommonStoreStub } from '@robby-rabbitman/cv-libs-common-data/testing';
+import { SelectColorSchemeHarness } from '@robby-rabbitman/cv-libs-common-feature--color-scheme/testing';
 import { assertA11y } from '@robby-rabbitman/cv-libs-web-util';
 import { Header } from './header';
 
@@ -37,5 +39,13 @@ describe('[Component Test] Header', () => {
     const fixture = TestBed.createComponent(Header);
 
     await expectAsync(assertA11y(fixture.nativeElement)).not.toBeRejected();
+  });
+
+  it('should have a color scheme select', async () => {
+    const fixture = TestBed.createComponent(Header);
+    const loader = TestbedHarnessEnvironment.loader(fixture);
+    const selectColorScheme = await loader.getHarness(SelectColorSchemeHarness);
+
+    expect(selectColorScheme).toBeTruthy();
   });
 });
