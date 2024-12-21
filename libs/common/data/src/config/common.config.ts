bb9cda5e5e5be9d7c888e2ec0a7ec6a5118b87c9
Robby Rabbitman <robby.rabbitman+github@gmail.com>
2024-12-21 15:21:57 +0100

    refactor(@robby-rabbitman/cv-libs-common-data): provide features (#57)
    Co-authored-by: robby rabbitman <robby.rabbitman@gmail.com>

--- libs/common/data/src/config/common.config.ts
+++ libs/common/data/src/config/common.config.ts
@@ -1,7 +1,50 @@
-import { makeEnvironmentProviders } from '@angular/core';
-import { withColorScheme } from './with-color-scheme';
-import { withCommonStore } from './with-common-store';
+import {
+  inject,
+  makeEnvironmentProviders,
+  provideEnvironmentInitializer,
+  type EnvironmentProviders,
+  type Provider,
+} from '@angular/core';
+import {
+  setColorScheme,
+  type ColorSchemeOptions,
+} from '@robby-rabbitman/cv-libs-common-util';
+import { CommonStore } from '../store/common.store';
 
-export function provideCommonData() {
-  return makeEnvironmentProviders([withCommonStore(), withColorScheme()]);
+export function provideCommonData(...features: CommonDataFeature[]) {
+  return makeEnvironmentProviders(features.map((feature) => feature.providers));
+}
+
+export type CommonDataFeatureKind = 'color-scheme' | 'store';
+
+export interface CommonDataFeature {
+  kind: CommonDataFeatureKind;
+  providers: (Provider | EnvironmentProviders)[];
+}
+
+export function makeCommonDataFeature(
+  kind: CommonDataFeatureKind,
+  providers: (Provider | EnvironmentProviders)[],
+) {
+  return { kind, providers } satisfies CommonDataFeature;
+}
+
+/** Sets the color scheme based on the state of the common store. */
+export function withColorScheme(
+  options?: Pick<ColorSchemeOptions, 'targetElement'>,
+) {
+  return makeCommonDataFeature('color-scheme', [
+    provideEnvironmentInitializer(() => {
+      const commonStore = inject(CommonStore);
+      setColorScheme({
+        colorScheme: commonStore.colorScheme,
+        targetElement: options?.targetElement,
+      });
+    }),
+  ]);
+}
+
+/** Provides the {@link CommonStore}. */
+export function withCommonStore() {
+  return makeCommonDataFeature('store', [CommonStore]);
 }
--- libs/common/data/src/config/with-common-store.ts
+++
@@ -1,7 +0,0 @@
-import { makeEnvironmentProviders } from '@angular/core';
-import { CommonStore } from '../store/common.store';
-
-/** Provides the {@link CommonStore}. */
-export function withCommonStore() {
-  return makeEnvironmentProviders([CommonStore]);
-}
