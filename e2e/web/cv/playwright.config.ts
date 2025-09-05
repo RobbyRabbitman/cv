563e4faece0d243fd205e3fb7ae1c404f68b7e38
robby rabbitman <robby.rabbitman@gmail.com>
2025-09-05 18:14:29 +0200

    basic e2e

--- e2e/web/cv/playwright.config.ts
+++ e2e/web/cv/playwright.config.ts
@@ -1,14 +1,20 @@
+import cvWebApp from '@robby-rabbitman/cv-apps-web-cv/package.json' with { type: 'json' };
 import { playwrightConfig } from '@robby-rabbitman/cv-tools-playwright';
 
 const CI = !!process.env.CI;
-const baseURL = !CI ? 'http://localhost:4200/' : undefined;
+const baseURL = !CI
+  ? `http://localhost:${cvWebApp.nx.targets.serve.options.port}/`
+  : undefined;
 
 export default playwrightConfig({
   use: {
     baseURL,
   },
   webServer: {
-    command: 'pnpm nx run @robby-rabbitman/cv-apps-web-cv:serve',
+    name: cvWebApp.name,
+    command: `pnpm nx run ${cvWebApp.name}:serve`,
     url: baseURL,
+    timeout: 120_000,
+    reuseExistingServer: !CI,
   },
 });
