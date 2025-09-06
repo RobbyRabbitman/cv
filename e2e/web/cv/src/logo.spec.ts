478a6fb2759d6cf8fdfa52f8dd74730b1a6108a4
robby rabbitman <robby.rabbitman@gmail.com>
2025-09-06 18:51:28 +0200

    ref

--- e2e/web/cv/src/logo.spec.ts
+++ e2e/web/cv/src/logo.spec.ts
@@ -1,6 +1,12 @@
 import { LogoHarness } from '@robby-rabbitman/cv-libs-common-features-shell/testing';
 import { expect, test } from '@robby-rabbitman/cv-tools-playwright-harness';
 
+test('has title', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+  await expect(page).toHaveTitle(/Playwright/);
+});
+
 test('has logo', async ({ harness }) => {
   const logo = await harness.loader.getHarness(LogoHarness);
 
