f555ebbaebdc323354725b73642cd1e666e5c9fb
robby rabbitman <robby.rabbitman@gmail.com>
2025-09-08 21:01:27 +0200

    ref

--- libs/i18n/types/src/translations.ts
+++ libs/i18n/types/src/translations.ts
@@ -3,6 +3,8 @@ import type { LocaleId } from './locale';
 
 /** Translations of a certain locale. */
 export interface Translations extends Identifiable {
+  /** The associated locale id of these translations. */
   localeId: LocaleId;
+
   value: Record<string, unknown>;
 }
