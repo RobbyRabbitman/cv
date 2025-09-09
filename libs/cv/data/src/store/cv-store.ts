c26f53079519e4d46308193e82a7b84727a67c69
robby rabbitman <robby.rabbitman@gmail.com>
2025-09-09 21:39:46 +0200

    base store

--- libs/cv/data/src/store/cv-store.ts
+++ libs/cv/data/src/store/cv-store.ts
@@ -1,16 +1,50 @@
-import { signalStore, type } from '@ngrx/signals';
-import { withEntities } from '@ngrx/signals/entities';
+import { signalStore, type, withLinkedState, withState } from '@ngrx/signals';
+import { entityConfig, withEntities } from '@ngrx/signals/entities';
+import type { UUID } from '@robby-rabbitman/cv-libs-common-types';
 import {
   type BlockPrototype,
   type Cv,
   type CvTemplate,
 } from '@robby-rabbitman/cv-libs-cv-types';
 
+interface CvState {
+  _activeId: UUID | null | undefined;
+}
+
+const CvEntity = entityConfig({
+  entity: type<Cv>(),
+  collection: '_cv',
+  selectId: (cv) => cv.id,
+});
+
+const TemplateEntity = entityConfig({
+  entity: type<CvTemplate>(),
+  collection: '_template',
+  selectId: (template) => template.id,
+});
+
+const PrototypeEntity = entityConfig({
+  entity: type<BlockPrototype>(),
+  collection: '_prototype',
+  selectId: (prototype) => prototype.id,
+});
+
 export const CvStore = signalStore(
-  withEntities({ entity: type<Cv>(), collection: 'cv' }),
-  withEntities({
-    entity: type<CvTemplate>(),
-    collection: 'template',
-  }),
-  withEntities({ entity: type<BlockPrototype>(), collection: 'prototype' }),
+  withState<CvState>({ _activeId: undefined }),
+  withEntities(CvEntity),
+  withEntities(TemplateEntity),
+  withEntities(PrototypeEntity),
+  withLinkedState(({ _activeId, _cvEntityMap }) => ({
+    active: () => {
+      const activeId = _activeId();
+
+      if (!activeId) {
+        return null;
+      }
+
+      const cvs = _cvEntityMap();
+
+      return cvs[activeId];
+    },
+  })),
 );
--- libs/cv/features/documents/src/pages/cv-documents.ts
+++ libs/cv/features/documents/src/pages/cv-documents.ts
@@ -1,7 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { CvStore } from '@robby-rabbitman/cv-libs-cv-data';
 
 @Component({
   selector: 'cv-documents',
   template: `cv documents works!`,
 })
-export default class CvDocuments {}
+export default class CvDocuments {
+  protected cv = inject(CvStore);
+}
