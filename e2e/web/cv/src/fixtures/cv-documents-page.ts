581cd37fb264e5645a95308925f8d7b49fb40b6e
robby rabbitman <robby.rabbitman@gmail.com>
2025-09-27 11:19:13 +0200

    layout cvs

--- e2e/web/cv/src/fixtures/cv-documents-page.ts
+++ e2e/web/cv/src/fixtures/cv-documents-page.ts
@@ -3,13 +3,17 @@ import type { Shell } from './shell.js';
 
 export class CvDocumentsPage {
   protected readonly createButton: Locator;
+  protected readonly cvList: Locator;
+  protected readonly cv: Locator;
 
   constructor(protected readonly shell: Shell) {
     this.createButton = shell.main.getByRole('button', { name: 'Create CV' });
+    this.cvList = shell.main.getByRole('list');
+    this.cv = this.cvList.getByRole('listitem');
   }
 
   getCv(name: string | RegExp) {
-    return this.shell.main.getByRole('button', { name });
+    return this.cv.getByRole('button', { name });
   }
 
   async goTo() {
--- libs/cv/features/documents/src/components/cv-documents.ts
+++ libs/cv/features/documents/src/components/cv-documents.ts
@@ -16,15 +16,15 @@ import { TranslatePipe } from '@robby-rabbitman/cv-libs-i18n-features-translatio
   host: {
     role: 'list',
     class:
-      'list-none grid grid-cols-1 min-[35rem]:grid-cols-2 md:grid-cols-[repeat(auto-fill,minmax(20rem,1fr))] gap-6',
+      'list-none grid grid-cols-1 min-[35rem]:grid-cols-2 md:grid-cols-[repeat(auto-fit,minmax(18rem,1fr))] gap-6',
   },
   styleUrl: './cv-documents.scss',
   template: `@for (cv of cv.all.value(); track cv.id) {
     @let descriptionId = 'cv--documents__description--' + cv.id;
-    <li class="w-fit">
+    <li>
       <button
         matButton="outlined"
-        class="aspect-square"
+        class="aspect-square w-full"
         [attr.aria-label]="
           'cv.documents.edit_button.text' | translate: { name: cv.label }
         "
